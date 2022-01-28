93ed05b9ee05152f83bec3d79431a2cea09cd056
Nicholas Li <canyu2007@gmail.com>
2022-01-27 20:02:56 -0700

    Finished Rising Action

--- src/game/chapters/chapter2.js
+++ src/game/chapters/chapter2.js
@@ -1,5 +1,18 @@
 let ch2 = [];
 
 //Add theater image
+let ch2_15 = new Scene("Yessir.","Will","theater",ch2,[]);
+let ch2_14 = new Scene("Go gather up the theater kids instead of wasting air, would you?","nThomas","theater",ch2,[ch2_15]);
+let ch2_13 = new Scene("You were too busy thinking about something else. Or should I say, someone else. By the way, nice to meet you, $n. I’ve heard so much about you. Thomas told me he—","Will","theater",ch2,[ch2_14]);
+let ch2_12 = new Scene("O-oh, yeah, you’re totally right, Will. Why didn’t I think of that?","hThomas","theater",ch2,[ch2_13]);
+let ch2_11 = new Scene("Why don’t we put it to a vote? This play ain’t just about you two lovebirds.","Will","theater",ch2,[ch2_12]);
+let ch2_10 = new Scene("I-I like both of them, though.","hThomas","theater",ch2,[ch2_11]);
+let ch2_9 = new Scene("I’d prefer to let you decide. I wanna know more about you— I mean, your, uh, tastes.","hThomas","theater",ch2,[ch2_10]);
+let ch2_8 = new Scene("I’ve got two potential plots so far. A modern murder mystery and a period romance. Which one is more your taste?","hThomas","theater",ch2,[ch2_9]);
+let ch2_7 = new Scene("How’s it coming?","hThomas","theater",ch2,[ch2_8]);
+let ch2_6 = new Scene("Ok! I’ll set up some rough plots, then. Brb.","hThomas","theater",ch2,[ch2_7]);
+let ch2_5 = new Scene("Y-yeah, tubular, man. I’m totally down for that.","hThomas","theater",ch2,[ch2_6]);
+let ch2_4 = new Scene("Ooh! My favorite! I’ve got tons of ideas for that. Um… they’re all romance stories. Is that ok?","hThomas","theater",ch2,[ch2_5]);
+let ch2_3 = new Scene("It’s going to be some flavor of fantasy.","hThomas","theater",ch2,[ch2_4]);
 let ch2_2 = new Scene("What's the genre of the play?","hThomas","theater",ch2,[ch2_3]);
 let ch2_1 = new Scene("$n! So glad to see you again! Let’s get started.","hThomas","theater",ch2,[ch2_2]);
