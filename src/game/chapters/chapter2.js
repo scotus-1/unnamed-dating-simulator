cbd9870a66b0309e326c3a1a29be31f4405fe8b4
Nicholas Li <canyu2007@gmail.com>
2022-01-27 19:39:00 -0700

    Added two scenes in chapter 2

--- src/game/chapters/chapter2.js
+++ src/game/chapters/chapter2.js
@@ -1,1 +1,5 @@
 let ch2 = [];
+
+//Add theater image
+let ch2_2 = new Scene("What's the genre of the play?","hThomas","theater",ch2,[ch2_3]);
+let ch2_1 = new Scene("$n! So glad to see you again! Let’s get started.","hThomas","theater",ch2,[ch2_2]);
